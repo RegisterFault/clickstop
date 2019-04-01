b7d55f4ca4e38869d0df9135602d103a507063c1
travis <travis@localhost.localdomain>
2019-04-01 10:47:25 -0400

    initial addition of primary script from tinkering

---
+++ clickstop.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+#Click on an x window to pause or resume
+#  This recursively obtains the child process tree of the whole application
+#requires xdotool, pgrep
+DEBUG=true
+
+if [ "$1" == "pause" ]
+then
+	KILLARG="-STOP"
+elif [ "$1" == "resume" ]
+then
+	KILLARG="-CONT"
+else
+	echo "ERROR: Commands are \"pause\" or \"resume\" only!" > /dev/stderr
+	exit
+fi
+
+
+
+children() {
+	PIDNODES=$( for i in $@
+                    do
+                            pgrep -P $i
+                    done)
+
+	#empty pidnodes means recursive exit condition
+	if [ "$PIDNODES" ]
+	then
+		echo $PIDNODES
+		children $PIDNODES
+	fi
+}
+
+PROC=$(xdotool selectwindow getwindowpid)
+[ "$DEBUG" == "true" ] && echo $PROC > /dev/stderr
+CHILDREN=$(children $PROC)
+[ "$DEBUG" == "true" ] && echo $CHILDREN > /dev/stderr
+PROCS="$PROC $CHILDREN"
+[ "$DEBUG" == "true" ] && echo $PROCS > /dev/stderr
+echo $PROCS
+
+
+for i in $PROCS
+do
+	[ "$DEBUG" == "true" ] && echo "kill $KILLARG $i" > /dev/stderr
+	kill $KILLARG $i &
+done
+
+
